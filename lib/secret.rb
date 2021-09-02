95393ce9701e9122433a08217d6d0ffba5bde104
Billy Frey <bfrey16@gmail.com>
2021-09-01 18:53:17 -0600

    feat: add shuffle_4 method to secret class

--- lib/secret.rb
+++ lib/secret.rb
@@ -1,2 +1,13 @@
 class Secret
-end 
+  attr_reader :colors_4, :colors
+  def initialize(colors)
+    @colors = colors
+    @colors_4 = []
+  end
+  def shuffle_4
+    4.times do
+      @colors_4 << colors.shuffle.pop
+    end
+    return @colors_4
+  end
+end
