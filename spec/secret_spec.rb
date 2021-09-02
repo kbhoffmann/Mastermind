8a46ae81570ff768988c3c0d18f70410b5c51a14
Billy Frey <bfrey16@gmail.com>
2021-09-01 18:54:32 -0600

    test: add secret shuffle_4 test

--- spec/secret_spec.rb
+++ spec/secret_spec.rb
@@ -3,9 +3,19 @@ require './lib/secret'
 
 RSpec.describe Secret do
   it "exists" do
+  colors = ["blue", "green", "yellow", "red", "blue", "green", "yellow", "red", "blue", "green", "yellow", "red", "blue", "green", "yellow", "red", "blue", "green", "yellow", "red"]
+
+  secret_1 = Secret.new(colors)
 
 
-  secret_1 = Secret.new
   expect(secret_1).to be_an_instance_of(Secret)
+  end
+
+  it "can return shuffle_4 which is 4 colors in random order" do
+
+    colors = ["blue", "green", "yellow", "red", "blue", "green", "yellow", "red", "blue", "green", "yellow", "red", "blue", "green", "yellow", "red", "blue", "green", "yellow", "red"]
+    secret_1 = Secret.new(colors)
+
+    expect(secret_1.shuffle_4.length).to eq(4)
   end
 end
