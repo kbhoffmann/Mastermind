32c12072b20699413f1ea9199a857c24d4e96586
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-03 17:02:23 -0600

    Feat: add user input methods

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,3 +1,5 @@
+require "./spec/secret_spec.rb"
+
 class Guess
   attr_reader :user_input
   def initialize (user_input)
@@ -6,5 +8,25 @@ class Guess
 
   def check_length?
     user_input.length == 4
+  end
+
+  def correct_format
+    user_input.split("")
+  end
+
+  def is_quit?
+    user_input == "q"
+  end
+
+  def is_instructions?
+    user_input == "i"
+  end
+
+  def is_play?
+    user_input == "p"
+  end
+
+  def is_cheat?
+    user_input == "c"
   end
 end
