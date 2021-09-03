db5f4f12705998083acb0e67dfd6883e6308e96b
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-02 20:44:44 -0600

    Test: add shortened method test

--- spec/secret_spec.rb
+++ spec/secret_spec.rb
@@ -17,5 +17,14 @@ RSpec.describe Secret do
     secret_1 = Secret.new(colors)
 
     expect(secret_1.shuffle_4.length).to eq(4)
+    expect(secret_1.shuffle_4).to be_a(Array)
+  end
+
+  it "can shorten colors to single letter" do
+
+    secret_2 = Secret.new(['red', 'green', 'yellow', 'blue'])
+
+    secret_2.shuffle_4
+    expect(secret_2.shortened[0].length).to eq(1)
   end
 end
