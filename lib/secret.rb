c41f9d0ab0935796a97530c00d542f3da58c9152
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-02 20:44:09 -0600

    Feat: add shortened method

--- lib/secret.rb
+++ lib/secret.rb
@@ -4,10 +4,21 @@ class Secret
     @colors = colors
     @colors_4 = []
   end
+
   def shuffle_4
     4.times do
       @colors_4 << colors.shuffle.pop
     end
     return @colors_4
+  end
+
+  def shortened
+    colors_first_letter = []
+
+    @colors_4.each do |color|
+      
+      colors_first_letter << color[0]
+    end
+    return colors_first_letter
   end
 end
