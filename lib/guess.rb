c8b20a4875611a1a9a1b09f869d0b21623531c40
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-05 14:18:06 -0600

    Feat: add guess length feedback.

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,4 +1,3 @@
-require "./spec/secret_spec.rb"
 
 class Guess
   attr_reader :user_input
@@ -6,27 +5,45 @@ class Guess
     @user_input = user_input
   end
 
+  puts "What is your first guess?"
+  @user_input = $stdin.gets.chomp
+
   def check_length?
-    user_input.length == 4
+    @user_input.length == 4
+  end
+
+  def guess_length
+    # if @user_input.length == 4
+    #   #enter game play
+    if @user_input.length > 4
+      puts "Guess is too long, it must be 4 letters long. Try again."
+      puts '<'
+      @user_input = $stdin.gets.chomp
+
+    elsif @user_input.length < 4
+      puts "Guess is too short, it must be 4 letters long. Try again."
+      puts '<'
+      @user_input = $stdin.gets.chomp
+    end
   end
 
   def correct_format
-    user_input.split("")
+    @user_input.split("")
   end
 
   def is_quit?
-    user_input == "q"
+    @user_input == "q"
   end
 
   def is_instructions?
-    user_input == "i"
+    @user_input == "i"
   end
 
   def is_play?
-    user_input == "p"
+    @user_input == "p"
   end
 
   def is_cheat?
-    user_input == "c"
+    @user_input == "c"
   end
 end
