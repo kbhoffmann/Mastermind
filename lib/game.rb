c3847934de26879e1ea901b2a23565716b9bc2e3
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-05 13:38:41 -0600

    Feat: add time tracking methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,27 @@
-
 class Game
   def initialize
   end
-  def welcome
-    welcome_message = "Welcome to MASTERMIND\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
-    puts welcome_message
-    return welcome_message
+
+  # def cheat
+  #   # if user_input == 'c' || user_input == 'cheat'
+  #     puts secret_2.shortened
+  #
+  # end
+
+  #starts tracking time when user enters first guess
+  def start_time
+    Time.new.round(2)
   end
+
+  #stops tracking time when user wins
+  #added 1 to make sure time_elapsed method worked
+  def end_time
+    Time.new.round(2)
+  end
+
+  #calculates total time user took to guess secret, prints with winning message
+  def time_elapsed
+    end_time - start_time
+  end
+
 end
