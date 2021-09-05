e8ebf6435f9a8e7442f6b78773333b913517f16b
Billy Frey <bfrey16@gmail.com>
2021-09-05 10:28:43 -0600

    feat: add print feedback method to feedback class and update variable names for better understanding

--- lib/feedback.rb
+++ lib/feedback.rb
@@ -1,12 +1,11 @@
 
 class Feedback
-  attr_reader :counter, :secret, :guess, :counter_order, :counter_no_order
+  attr_reader :counter, :secret, :guess, :counter_order
   def initialize(secret, guess)
     @secret = secret
     @guess = guess
     @counter_order = 0
-    @counter_no_order = 0
-    @counter_no_order_num = []
+    @no_order_uniq_array = []
   end
 
   def compare_order
@@ -18,21 +17,22 @@ class Feedback
         @counter_order += 0
       end
     end
+    @counter_order
   end
 
   def compare_no_order
-    require 'pry' ; binding.pry
     secret.shuffle_4
     guess.correct_format.uniq.find_all do |i|
-       @counter_no_order_num << ((secret.colors_4).find {|x| i == x})
+       @no_order_uniq_array << ((secret.colors_4).find {|x| i == x})
     end
-     @counter_no_order_num = @counter_no_order_num.compact
-     @counter_no_order_num.length
-
-
-
+     @no_order_uniq_array = @no_order_uniq_array.compact
+     @no_order_uniq_array.length
   end
 
+  def print_feedback
+    puts "'#{guess.user_input.join.upcase}' has #{self.compare_no_order} of the correct elements with #{self.compare_order} in the correct positions\nYou've taken #{guess.count} guess"
+
+  end
 
 
 
