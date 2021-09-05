fa7ac96a4b2014a949e4b1190d4e032f65f3e392
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-05 13:50:28 -0600

    Feat: edit typing errors

--- mastermind.rb
+++ mastermind.rb
@@ -1,8 +1,8 @@
 require 'colorize'
-# require 'pry'
-# require './lib/guess'
-# require './lib/secret'
-# require "./spec/secret_spec.rb"
+require 'pry'
+require './lib/guess'
+require './lib/secret'
+require './lib/game'
 
 welcome_message = "Welcome to MASTERMIND\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
 
@@ -38,7 +38,7 @@ elsif user_input == 'q' || user_input == 'quit'
 elsif user_input == 'i' || user_input == 'instructions'
 
   puts instructions
-  puts "Would you like to contintinue to p(lay) or q(uit)?"
+  puts "Would you like to contintinue to (p)lay or (q)uit?"
 
   user_input = $stdin.gets.chomp
 
@@ -54,7 +54,5 @@ elsif user_input == 'i' || user_input == 'instructions'
     exit!
 
   end
-elsif user_input == 'c' || user_input == 'cheat'
-  puts computer_secret
 
 end
