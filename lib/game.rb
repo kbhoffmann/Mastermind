3ecc1659687c9116af128a5bd6ec1b0d0dec5819
Billy Frey <bfrey16@gmail.com>
2021-09-06 15:57:14 -0600

    feat: add play method and cheat method with initializing a new secret with every game

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,7 @@
 class Game
   def initialize
+    @colors = ["blue", "green", "yellow", "red", "blue", "green", "yellow", "red", "blue", "green", "yellow", "red", "blue", "green", "yellow", "red", "blue", "green", "yellow", "red"]
+    @secret = Secret.new(@colors)
   end
 
   # def cheat
@@ -8,43 +10,95 @@ class Game
   #
   # end
 
-  puts "What is your first guess?"
-  guess = $stdin.gets.chomp
-  #starts tracking time when user enters first guess
+  def play
+    @secret.shuffle_4
+    user_input = gets.chomp
+    guess = Guess.new(user_input)
+    if guess.check_length?
+      feedback = Feedback.new(@secret, guess)
+      feedback.print_feedback
+    elsif guess.is_quit?
+      self.quit
+    elsif guess.is_instructions?
+      self.instructions
+    elsif guess.is_cheat?
+      self.cheat
+    else
+    end
 
-  def start_time
-    Time.new.round(2)
+    until feedback.is_win? || guess.is_cheat? || guess.is_quit?
+
+      user_input = gets.chomp
+      guess = Guess.new(user_input)
+      if guess.check_length?
+        feedback = Feedback.new(@secret, guess)
+        feedback.print_feedback
+
+      elsif guess.is_quit?
+        self.quit
+      elsif guess.is_instructions?
+        self.instructions
+      elsif guess.is_cheat?
+        self.cheat
+      else
+      end
+    end
+
   end
 
-  #stops tracking time when user wins
-  #added 1 to make sure time_elapsed method worked
-  def end_time
-    Time.new.round(2)
+  def cheat
+    puts @secret.colors_4
   end
 
-  #calculates total time user took to guess secret, prints with winning message
-  def time_elapsed
-    end_time - start_time
+  def instructions
+  end
+
+  def quit
   end
 
 
-  if guess == 'q' || guess == 'quit'
-    puts "Thank you for checking out Mastermind!  Goodbye"
-    exit!
-  end
 
-  if guess.length == 4
-    #enter comparison/feedback mode
 
-  elsif guess.length < 4
-    puts "Guess is too short, it must be 4 letters long. Try again."
-    puts '<'
-      $stdin.gets.chomp
 
-  elsif guess.length > 4
-    puts "Guess is too long, it must be 4 letters long. Try again."
-    puts '<'
-      $stdin.gets.chomp
-  end
+
+  #
+  # puts "What is your first guess?"
+  # guess = gets.chomp
+  # #starts tracking time when user enters first guess
+  #
+  # def start_time
+  #   Time.new.round(2)
+  # end
+  #
+  # #stops tracking time when user wins
+  # #added 1 to make sure time_elapsed method worked
+  # def end_time
+  #   Time.new.round(2)
+  # end
+  #
+  # #calculates total time user took to guess secret, prints with winning message
+  # def time_elapsed
+  #   end_time - start_time
+  # end
+  #
+  #
+  # if guess == 'q' || guess == 'quit'
+  #   puts "Thank you for checking out Mastermind!  Goodbye"
+  #   exit!
+  # end
+  #
+  # if guess.length == 4
+  #   #enter comparison/feedback mode
+  #
+  # elsif guess.length < 4
+  #   puts "Guess is too short, it must be 4 letters long. Try again."
+  #   puts '<'
+  #     $stdin.gets.chomp
+  #
+  # elsif guess.length > 4
+  #   puts "Guess is too long, it must be 4 letters long. Try again."
+  #   puts '<'
+  #     $stdin.gets.chomp
+  # end
 
 end
