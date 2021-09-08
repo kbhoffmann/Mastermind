aa057568f1243377a0960b1e39bdfe6676cebad7
Billy Frey <bfrey16@gmail.com>
2021-09-07 23:25:25 -0600

    fix: change guess count variable to global variable to easily reset when user plays a new game in same instance

--- lib/feedback.rb
+++ lib/feedback.rb
@@ -38,9 +38,9 @@ class Feedback
     if self.is_win? == true
       $time_end = Time.new.round(2)
       $time = $time_end - $time_start
-      puts "Congratulations! You guessed the sequence '#{guess.correct_format.join.upcase}' in #{guess.count} guesses over #{$time} seconds\nDo you want to (p)lay again or (q)uit?"
+      puts "Congratulations! You guessed the sequence '#{guess.correct_format.join.upcase}' in #{$count} guesses over #{$time} seconds\nDo you want to (p)lay again or (q)uit?"
     else
-    puts "'#{guess.correct_format.join.upcase}' has #{self.compare_no_order} of the correct elements with #{self.compare_order} in the correct positions\nYou've taken #{guess.count} guess"
+    puts "'#{guess.correct_format.join.upcase}' has #{self.compare_no_order} of the correct elements with #{self.compare_order} in the correct positions\nYou've taken #{$count} guess"
     end
   end
 
