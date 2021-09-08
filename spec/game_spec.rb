42f195891e86597654948bdc63cc4ff14e70737d
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-08 16:16:39 -0600

    Test: add test for cheat method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,51 +2,20 @@ require 'pry'
 require './lib/game'
 require './lib/guess'
 require './lib/secret'
-# require './mastermind'
 require './lib/feedback'
 
 RSpec.describe Game do
+
   it "exists" do
     game = Game.new
     expect(game).to be_an_instance_of(Game)
   end
 
-  # it "can let a user cheat" do
-  #   game = Game.new
-  #   secret_2 = Secret.new('rgyb')
-  #   expect(game.cheat).to eq(secret_2.shortened)
-  # end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  it "can start tracking time" do
+  it "can let a user cheat" do
     game = Game.new
-    p game.start_time
+    secret_2 = Secret.new('rgyb')
+    expect(game.cheat).to eq(secret_2.shortened.join.upcase)
   end
 
-  it "can stop tracking time" do
-    game = Game.new
-    p game.end_time
-  end
-
-  it "can calculate time elapsed" do
-    game = Game.new
-    p game.time_elapsed
-  end
 
 end
